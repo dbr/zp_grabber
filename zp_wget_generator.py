a90f75adf1e44958502b98fed293d92fa43666b6
dbr <dbr.onix@gmail.com>
2009-09-01 20:12:04 +0100

    Tidy up zp_wget_generator, use ZpCacher class rather than re-implimenting the cache parser

--- zp_wget_generator.py
+++ zp_wget_generator.py
@@ -10,37 +10,21 @@ $ ./grabber.sh # grab all Zero Punctuation episodes into current directory
 Use sanely
 """
 import re
+from zp_grabber import ZpCacher
 
-def sort_nicely( l ): 
+def sort_nicely(l): 
     """
     Sort the given list in the way that humans expect. 
     """ 
     convert = lambda text: int(text) if text.isdigit() else text 
-    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
-    l.sort( key=alphanum_key )
+    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
+    l.sort(key=alphanum_key)
     return l
 
-fp = open("zp_cache.txt")
-lines = fp.readlines()
+zpc = ZpCacher()
+videos = zpc.cache
 
-videos = {}
-
-for current_line in lines:
-    try:
-        c_vid, c_flv_url, c_web_url, c_title = [x.strip() for x in current_line.split("|")]
-        videos[c_vid] = {
-            'flv':c_flv_url,
-            'web':c_web_url,
-            'vid':c_vid,
-            'title':c_title
-        }
-    except ValueError:
-        print "invalid line %s" % (current_line)
-        continue # Invalid line, skip
-
-counter = 1
-for vid in sort_nicely(videos.keys()):
+for counter, vid in enumerate(sort_nicely(videos.keys())):
     cur = videos[vid]
     new_name = "Zero Punctuation - [%02d] - %s.flv" % (counter, cur['title'])
     print "wget -c %s -O \"%s\"" % (cur['flv'], new_name)
-    counter += 1
