728c95bff5410780647e025412caed84f1fde068
dbr <dbr.onix@gmail.com>
2011-01-15 16:13:29 +1030

    Ensure output filenames are valid, and replace "Blah: blah" with "Blah - blah"

--- zp_ipod.py
+++ zp_ipod.py
@@ -17,6 +17,7 @@ play back on most devices (including iPhone, Xbox 360 etc)
 """
 import os
 import re
+import platform
 import subprocess
 from urlparse import urlparse
 
@@ -108,6 +109,103 @@ class Transcoder:
         cmd += ["-i", self.fname, "-o", outname]
         return cmd
 
+def makeValidFilename(value, normalize_unicode = False, windows_safe = False, custom_blacklist = None, replace_with = "_"):
+    """
+    Takes a string and makes it into a valid filename.
+
+    normalize_unicode replaces accented characters with ASCII equivalent, and
+    removes characters that cannot be converted sensibly to ASCII.
+
+    windows_safe forces Windows-safe filenames, regardless of current platform
+
+    custom_blacklist specifies additional characters that will removed. This
+    will not touch the extension separator:
+
+        >>> makeValidFilename("T.est.avi", custom_blacklist=".")
+        'T_est.avi'
+    """
+
+    if windows_safe:
+        # Allow user to make Windows-safe filenames, if they so choose
+        sysname = "Windows"
+    else:
+        sysname = platform.system()
+
+    # If the filename starts with a . prepend it with an underscore, so it
+    # doesn't become hidden.
+
+    # This is done before calling splitext to handle filename of ".", as
+    # splitext acts differently in python 2.5 and 2.6 - 2.5 returns ('', '.')
+    # and 2.6 returns ('.', ''), so rather than special case '.', this
+    # special-cases all files starting with "." equally (since dotfiles have
+    # no extension)
+    if value.startswith("."):
+        value = "_" + value
+
+    # Treat extension seperatly
+    value, extension = os.path.splitext(value)
+
+    # Remove any null bytes
+    value = value.replace("\0", "")
+
+    # Blacklist of characters
+    if sysname == 'Darwin':
+        # : is technically allowed, but Finder will treat it as / and will
+        # generally cause weird behaviour, so treat it as invalid.
+        blacklist = r"/:"
+    elif sysname in ['Linux', 'FreeBSD']:
+        blacklist = r"/"
+    else:
+        # platform.system docs say it could also return "Windows" or "Java".
+        # Failsafe and use Windows sanitisation for Java, as it could be any
+        # operating system.
+        blacklist = r"\/:*?\"<>|"
+
+    # Append custom blacklisted characters
+    if custom_blacklist is not None:
+        blacklist += custom_blacklist
+
+    # Replace every blacklisted character with a underscore
+    value = re.sub("[%s]" % re.escape(blacklist), replace_with, value)
+
+    # Remove any trailing whitespace
+    value = value.strip()
+
+    # There are a bunch of filenames that are not allowed on Windows.
+    # As with character blacklist, treat non Darwin/Linux platforms as Windows
+    if sysname not in ['Darwin', 'Linux']:
+        invalid_filenames = ["CON", "PRN", "AUX", "NUL", "COM1", "COM2",
+        "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9", "LPT1",
+        "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9"]
+        if value in invalid_filenames:
+            value = "_" + value
+
+    # Replace accented characters with ASCII equivalent
+    if normalize_unicode:
+        import unicodedata
+        value = unicode(value) # cast data to unicode
+        value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
+
+    # Truncate filenames to valid/sane length.
+    # NTFS is limited to 255 characters, HFS+ and EXT3 don't seem to have
+    # limits, FAT32 is 254. I doubt anyone will take issue with losing that
+    # one possible character, and files over 254 are pointlessly unweidly
+    max_len = 254
+
+    if len(value + extension) > max_len:
+        if len(extension) > len(value):
+            # Truncate extension instead of filename, no extension should be
+            # this long..
+            new_length = max_len - len(value)
+            extension = extension[:new_length]
+        else:
+            # File name is longer than extension, truncate filename.
+            new_length = max_len - len(extension)
+            value = value[:new_length]
+
+    return value + extension
+
+
 def main():
     zpc = ZpCacher()
     zpc.load_cache()
@@ -144,6 +242,11 @@ def main():
             continue
 
         transcoded_name = "Zero Punctuation - [%02d] - %s.mp4" % (counter, cur['title'])
+
+        transcoded_name = transcoded_name.replace(": ", " - ")
+
+        transcoded_name = makeValidFilename(transcoded_name)
+
 
         print "Transcoding:"
         t = Transcoder(urlfilename)
